1b4e224d3c322e482c07bbd34c9a7f938dc05210
Sebastian Marzec <msebastian86@gmail.com>
2018-05-20 23:13:01 +0200

    another missing calculations, some fixes, visual upgrades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,15 +15,35 @@ export class AppComponent {
 	title:string = 'Investor App';
 	cenaZakupu:number = 243000;
 	rynekZakupu: string = '';
-	kwotaKredytu: number = 0;
-	wkladWlasny: number = 0;
-	prowizjaBanku: number = 0;
-	marzaBanku: number = 0;
-	lataKredytu: number = 0;
+	wkladWlasny: number = 72000;
+	kredytKwota: number;
+	prowizjaBanku: number = 2;
+	marzaBanku: number = 1.8;
+	lataKredytu: number = 30;
+	miesiaceKredytu: number = this.lataKredytu*12;
+	oprocentowanieWibor: number = 2;
 
+	kwotaKredytu(){
+		this.kredytKwota = this.cenaZakupu - this.wkladWlasny;
+		return this.kredytKwota;
+		
+	}
 
-	constructor() {}
-	ngOnInit() {}
+	kosztNotariusz(){
+		return this.cenaZakupu*0.03;
+	}
+
+	rataKredytu(){
+
+		let oprocentowanie = (this.oprocentowanieWibor + this.marzaBanku)/100;
+		let q = Math.pow(1 + oprocentowanie/12, this.miesiaceKredytu);
+		let rata = this.kredytKwota * q * ((1 + oprocentowanie/12 - 1) / (q-1));
+
+		return rata.toFixed(2);
+		// return miesiaceKredytu;
+
+	}
+
 
 
 
